770e44b77ca4d828ed675c1534265334bf475fdf
phileasdg <60669698+phileasdg@users.noreply.github.com>
2020-11-22 15:51:11 -0500

    Created readme.txt

--- GridworldAI.py
+++ GridworldAI.py
@@ -6,29 +6,48 @@ from matplotlib.patches import Rectangle
 import matplotlib.animation as animation
 import os
 
-# figure out what the agent does when it has no goals
-# the agent gets confused and doesn't move
+# This program is designed to facilitate AI agent testing in grid world environments. The program's functionality
+# includes the following:
+# - generating AI grid worlds at any scale.
+# - providing a framework to create and implement AI agents to interact in these grid world environments.
+# - providing a framework to visualise AI-environment interactions with data export capabilities.
+# - an example of AI agent implemented to interact with these grid worlds, with adjustable personal utility bias, utility
+# function, look ahear search, and start position.
 
-# the Agent has an environment to navigate, with goal(s) in the environment,
-# everywhere it goes, it changes the environment, and this can either be a neutral action (the
-# environment is not disturbed by the action of the AI, a positive action (the AI achieves a goal
-# and the environment is not disturbed), or negative (the environment is disturbed, or the move
-# shifts tha AI away from its goal, or makes makes it harder for the AI to achieve its goal).
+# HOW TO USE THIS PROGRAM
+# 1) by editing this master program and adding your own AI implementation
+# - create an environment object from the environment class and specify its size and whether it should be empty.
+# - run the environment object's get started method - optional: specify in attributes the agent start position and whether
+# # to save the gridworld plots as png files
+# - create an agent object from the agent class and specify its name and the dimensions of its environment.
+# - run the agent object's get started function
+# - run the agent object's run function
 
-# The agent is tasked with finding the most optimal ways of navigating this environment.
+# 2) by importing this program into your program
+# - add "import GridworldAI" to your python project header
+# - generate an environment by creating an environment object and following the directions specified above
+# - generate an agent by creating an agent object, or design an agent which takes as its input the environment object's
+# environment grid world.
+# - run the agent and study the results!
 
-# The environment cells can have values between -1 and 1, where 0 is neutral, -1 is negative, and 1 is positive)
+# DESCRIPTION
+# The Agent has an environment to navigate which contains goals and penalties. Every turn, it moves, and everywhere it
+# goes, it changes the environment. Every move is associated with a utility value which can be positive or negative.
+# Thus, an action can either be neutral (the environment is not disturbed by the action of the AI), a positive action
+# (the AI achieves or approaches a goal and the environment is not disturbed), or negative (the environment is disturbed,
+# or the move shifts tha AI away from its goal, or makes makes it harder for the AI to achieve its goal).
+# The agent is tasked with finding the most optimal ways of navigating this environment given specified environment configurations.
 
 class Environment:
 
-    def __init__(self, side_length, decimals=1, empty=None):
+    def __init__(self, side_length, decimals=1, empty=None, environment_utility_range_plusminus=0.5):
         self.side_length = side_length
         self.decimals = decimals
         if empty is True:
             self.grid_environment = np.zeros((side_length, side_length))  # empty grid
         else:
             # Environment grid: array of height == width == side_length; random values b/w -1 and 1, decimals = decimals.
-            self.grid_environment = np.around(np.random.uniform(-0.5, 0.5, (side_length, side_length)),
+            self.grid_environment = np.around(np.random.uniform(-environment_utility_range_plusminus, environment_utility_range_plusminus, (side_length, side_length)),
                                               decimals=decimals)  # decimal grid
             # self.grid = np.random.randint(-1, 1, (side_length, side_length))  # alternative integer grid
         self.goals = []
---
+++ readme.txt
@@ -0,0 +1,31 @@
+This program is designed to facilitate AI agent testing in grid world environments. The program's functionality
+includes the following:
+- generating AI grid worlds at any scale.
+- providing a framework to create and implement AI agents to interact in these grid world environments.
+- providing a framework to visualise AI-environment interactions with data export capabilities.
+- an example of AI agent implemented to interact with these grid worlds, with adjustable personal utility bias, utility
+function, look ahear search, and start position.
+
+HOW TO USE THIS PROGRAM
+1) by editing this master program and adding your own AI implementation
+- create an environment object from the environment class and specify its size and whether it should be empty.
+- run the environment object's get started method - optional: specify in attributes the agent start position and whether
+to save the gridworld plots as png files
+- create an agent object from the agent class and specify its name and the dimensions of its environment.
+- run the agent object's get started function
+- run the agent object's run function
+
+2) by importing this program into your program
+- add "import GridworldAI" to your python project header
+- generate an environment by creating an environment object and following the directions specified above
+- generate an agent by creating an agent object, or design an agent which takes as its input the environment object's
+environment grid world.
+- run the agent and study the results!
+
+DESCRIPTION
+The Agent has an environment to navigate which contains goals and penalties. Every turn, it moves, and everywhere it
+goes, it changes the environment. Every move is associated with a utility value which can be positive or negative.
+Thus, an action can either be neutral (the environment is not disturbed by the action of the AI), a positive action
+(the AI achieves or approaches a goal and the environment is not disturbed), or negative (the environment is disturbed,
+or the move shifts tha AI away from its goal, or makes makes it harder for the AI to achieve its goal).
+The agent is tasked with finding the most optimal ways of navigating this environment given specified environment configurations.
